4dbaf48f4904806fb1277cee3b77e1db06362413
CZH <13580518842@163.com>
2017-02-12 00:17:48 +0800

    test wechat MessageHandle 1.3

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -7,6 +7,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Log;
 use EasyWeChat\Foundation\Application;
+use EasyWeChat\Message;
 
 class WechatController extends Controller
 {
@@ -40,16 +41,30 @@ class WechatController extends Controller
 			switch ($msgType)
 			{
 				case 'text':
-					$msgResponse = "你发送的消息是:[".$message->Content."]";
+// 					$msgResponse = "你发送的消息是:[".$message->Content."]";
+					//方式一：
+					$msgResponse = new Message\Text(['content' => "你发送的消息是:[".$message->Content."]"]);
 					break;
 				case 'image':
-					$msgResponse = "你发送的图片是:[".$message->PicUrl."]";
+// 					$msgResponse = "你发送的图片是:[".$message->PicUrl."]";
+                    //方式二：
+					$msgResponse = new Message\Image();
+					$msgResponse->media_id = $message->MediaId;
 					break;
 				case 'voice':
 					$msgResponse = "你发送的语音id是:[".$message->MediaId."]以及格式是：[".$message->Format."]";
+					//方式二：
+					$msgResponse = new Message\Voice();
+					$msgResponse->media_id = $message->MediaId;
 					break;
 				case 'video':
-					$msgResponse = "你发送的视频id是:[".$message->MediaId."]以及缩略图媒体id是：[".$message->ThumbMediaId."]";
+// 					$msgResponse = "你发送的视频id是:[".$message->MediaId."]以及缩略图媒体id是：[".$message->ThumbMediaId."]";
+					//方式三：
+					$msgResponse = new Message\Video();
+					$msgResponse->setAttribute('title', '给回你的视频');
+					$msgResponse->setAttribute('description', '这是你的视频，现在给回你的视频');
+					$msgResponse->setAttribute('media_id', $message->MediaId);
+					$msgResponse->setAttribute('thumb_media_id', $message->ThumbMediaId);
 					break;
 				case 'shortvideo':
 					$msgResponse = "你发送的小视频id是:[".$message->MediaId."]以及缩略图媒体id是：[".$message->ThumbMediaId."]";
@@ -63,10 +78,19 @@ class WechatController extends Controller
 				case 'link':
 					$msgResponse = "你发送的消息标题是:[".$message->Title."]以及消息描述是：[".$message->Description."]以及消息链接是[".$message->Url.']';
 					break;
+				case 'event':
+					if ($message->Event == 'subscribe')
+					{
+						$msgResponse = "欢迎关注树多多事多，这里有很多东西等待你去开发";	
+					}else {
+						$message = null;
+					}
+					break;
 				default: 
 					$msgResponse = null;
 					break;
 			}
+			Log::info('make response.');
 			return $msgResponse;
 		});
 	
