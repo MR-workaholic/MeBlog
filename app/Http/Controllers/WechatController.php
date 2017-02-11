476bd94868564b7e4ca58c4ccd67563128ac99d7
CZH <13580518842@163.com>
2017-02-11 16:13:42 +0800

    test wechat MessageHandle

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -19,13 +19,13 @@ class WechatController extends Controller
 		return $response;
 	}
 	
-	public function serve()
+	public function handlemessage()
 	{
 		Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 	
 		$wechat = app('wechat');  //获取服务的方式一
 		$wechat->server->setMessageHandler(function($message){
-			return "欢迎关注 overtrue！";
+			return "欢迎关注  树多多事多！";
 		});
 	
 		Log::info('return response.');
